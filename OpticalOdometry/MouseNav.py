5bd1eed0e9772ba4518cd041f63eacf69bbebfe9
lucas <lucas>
2018-11-12 17:08:33 -0700

    hopefully fixed issues with calculating relative distance

--- OpticalOdometry/MouseNav.py
+++ OpticalOdometry/MouseNav.py
@@ -15,27 +15,22 @@ class MouseNav:
 		if(acc_LY == acc_RY):
 			return acc_LX, acc_LY, acc_RX, acc_RY, 0
 		else:
-			fixed_distance = 10.0
+			fixed_distance = 1.0
 			#if the center of rotation occures on either mouse the radius is equal to the fixed distance and we can simplify the math to either case
 			if(acc_RY == 0):
 				#theta in radians is equal to the arc (accumulated y) divided by the radius (fixed distance)
 				theta = acc_LY / fixed_distance
 				#solving a simple imaginary triangle gives us the y-part of the curve
 				left_y = fixed_distance * math.sin(theta)
-				#for the x-part we need the angle of the curve of our imaginary triangle
-				#the sum of the angles in all triangles is equal to pi and since the angles connecting our hypotonuse to the start and final
-				#points of the curve are equal, the angle we need can be found using this:
-				left_angle = ( math.pi / 2 ) - ( math.pi - theta ) / 2
-				#then we can solve for the x-part of the arc using the known y-part
-				left_x = left_y / math.tan(left_angle)
+				#for the x-part we need the hypotonuse of the imaginary triangle, this can be found using cosine law
+				left_x = math.pow( fixed_distance, 2 ) * (1 - math.cos( theta ) ) - left_y * left_y
 				right_y = 0
 				right_x = 0
 			elif(acc_LY == 0):
 				#see above
 				theta = -acc_RY / fixed_distance
 				right_y = fixed_distance * math.sin(theta)
-				right_angle = ( math.pi / 2 ) - ( math.pi - theta ) / 2
-				right_x = right_y / math.tan(right_angle)
+				right_x = math.pow( fixed_distance, 2 ) * (1 - math.cos( theta ) ) - right_y * right_y
 				left_y = 0
 				left_x = 0
 			else:
@@ -44,15 +39,12 @@ class MouseNav:
 				radius = (fixed_distance * acc_RY) / (acc_LY - acc_RY)
 				#angle measurments done in radians
 				theta = acc_LY / (fixed_distance + radius)
-				left_inner_angle = (math.pi - theta) / 2
-				right_inner_angle = (2 * math.pi - 2 * left_inner_angle) / 2
-				left_angle = (math.pi / 2) - left_inner_angle
-				right_angle = (math.pi / 2) - right_inner_angle
 
 				left_y = (fixed_distance + radius) * math.sin(theta)
-				left_x = left_y / math.tan(left_angle)
+				left_x = math.pow( fixed_distance + radius, 2) * ( 1 - math.cos( theta ) ) - left_y * left_y
 				right_y = radius * math.sin(theta)
-				right_x = right_y / math.tan(right_angle)
+				right_x = math.pow( fixed_distance, 2 ) * (1 - math.cos( theta ) ) - right_y * right_y
+
 			#now we add the accumulated x under the assumption that the x value was accumulated evenly accross the time of the arc
 			half_theta = theta / 2
 			#in theory acc_LX should always equal acc_RX or atleast be within 1-2 dots
